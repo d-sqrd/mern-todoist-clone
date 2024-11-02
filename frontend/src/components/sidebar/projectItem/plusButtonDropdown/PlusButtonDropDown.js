544baf7495de8b888cecc5f7ce54ec60599df59c
d-sqrd <deb.bunny98@gmail.com>
2024-11-02 21:28:19 +0530

    removed redundant CSS

--- frontend/src/components/sidebar/projectItem/plusButtonDropdown/PlusButtonDropDown.js
+++ frontend/src/components/sidebar/projectItem/plusButtonDropdown/PlusButtonDropDown.js
@@ -2,7 +2,7 @@ const PlusButtonDropDown = ({ isOpen }) => {
   if (!isOpen) return null;
   return (
     <div>
-      <div className="project__item__plus__btn__menu__container">
+      <div className="sidebar__acct__info__dropdown" style={{ top: "30px" }}>
         <div className="sidebar__btn__menu__dropdown__item">
           <p style={{ fontWeight: 500 }}>Add Project</p>
           <span>Plan and assign tasks</span>
