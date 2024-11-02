8b19ed357dfbc08e8199a5cd1420fcf7af780463
d-sqrd <deb.bunny98@gmail.com>
2024-11-02 13:55:06 +0530

    renamed CSS classes to prevent classname clashes

--- frontend/src/components/sidebar/projectItem/ProjectItem.js
+++ frontend/src/components/sidebar/projectItem/ProjectItem.js
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
+const subProjectItems = ["Sub Project Item 1", "Sub Project Item 2"];
+
 const ProjectItem = ({ projectDetails }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const handleArrowClick = () => {
@@ -10,20 +12,20 @@ const ProjectItem = ({ projectDetails }) => {
     setIsCollapsed((prevIsCollapsed) => !prevIsCollapsed);
   };
   return (
-    <div className="button__container">
+    <div className="project__item__button__container">
       {projectDetails}
       <div>
         <FontAwesomeIcon
           icon={faPlus}
           style={{ marginRight: "10px" }}
-          className="button__icons"
+          className="project__item__button__icons"
         />
         <FontAwesomeIcon
           icon={faAngleRight}
           className={
             isCollapsed
-              ? "button__icons arrow__clockwise__animate"
-              : "button__icons arrow__anti-clockwise__animate"
+              ? "project__item__button__icons project__item__arrow__clockwise__animate"
+              : "project__item__button__icons project__item__arrow__anti-clockwise__animate"
           }
           onClick={handleArrowClick}
         />
